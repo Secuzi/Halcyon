7b847801e762ea5cbb1a04634254de88012c3c7a
Harold <secuzi@gmail.com>
2025-03-11 21:01:34 +0800

    Changed fontattributes via fontfamily for labels

--- FilomenoMauiMidterm/MauiProgram.cs
+++ FilomenoMauiMidterm/MauiProgram.cs
@@ -14,6 +14,10 @@ namespace FilomenoMauiMidterm
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("Inter.ttf", "Inter");
+                    fonts.AddFont("Inter18ptLight.ttf", "InterLight");
+                    fonts.AddFont("Inter24ptMedium.ttf", "InterMedium");
+                    fonts.AddFont("Inter24ptSemiBold.ttf", "InterSemiBold");
+                    fonts.AddFont("Inter28ptBold.ttf", "InterBold");
                 });
 
 #if DEBUG
