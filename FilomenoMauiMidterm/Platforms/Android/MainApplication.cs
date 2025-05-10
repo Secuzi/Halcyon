1678f7a9aa0f0d684cf817e296ac93b638640ad8
Secuzi <secre2k@gmail.com>
2025-05-10 10:53:11 +0800

    feat: Added assembly code to allow users to pick images from their phones.

--- FilomenoMauiMidterm/Platforms/Android/MainApplication.cs
+++ FilomenoMauiMidterm/Platforms/Android/MainApplication.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Runtime;
-
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 32)]
 namespace FilomenoMauiMidterm
 {
     [Application]
