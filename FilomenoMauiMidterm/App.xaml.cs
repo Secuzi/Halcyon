97c21016e001cb6516b39001a331575be7939dd2
Rej Kyut <rejkyut@gmail.com>
2025-05-13 11:30:24 +0800

    static profile view

--- FilomenoMauiMidterm/App.xaml.cs
+++ FilomenoMauiMidterm/App.xaml.cs
@@ -10,8 +10,8 @@ namespace FilomenoMauiMidterm
             InitializeComponent();
 
 
-            //MainPage = new AppShell();
-            MainPage = new NavigationPage(new LoginView(loginViewModel, registerViewModel));
+            MainPage = new ProfileView();
+            //MainPage = new NavigationPage(new LoginView(loginViewModel, registerViewModel));
 #if ANDROID
            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
 			{
---
+++ FilomenoMauiMidterm/Views/ProfileView.xaml.cs
@@ -0,0 +1,9 @@
+namespace FilomenoMauiMidterm.Views;
+
+public partial class ProfileView : ContentPage
+{
+	public ProfileView()
+	{
+		InitializeComponent();
+	}
+}
