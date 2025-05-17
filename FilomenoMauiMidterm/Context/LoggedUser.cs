84055c925a4886425423272fd2b57a88cf66f288
Secuzi <secre2k@gmail.com>
2025-05-18 01:14:14 +0800

    feat: Added user for knowing who is logged in

---
+++ FilomenoMauiMidterm/Context/LoggedUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FilomenoMauiMidterm.Models;
+
+namespace FilomenoMauiMidterm.Context
+{
+    public class LoggedUser
+    {
+        public User User { get; set; }
+        public LoggedUser()
+        {
+            
+        }
+    }
+}
