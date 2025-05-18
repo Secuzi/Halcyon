e19b4991d2bec03561d47c6b6732e0460c517b98
Secuzi <secre2k@gmail.com>
2025-05-18 20:46:22 +0800

    feat: finished search page

---
+++ FilomenoMauiMidterm/ViewModels/SearchViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FilomenoMauiMidterm.Context;
+using FilomenoMauiMidterm.Models;
+using FilomenoMauiMidterm.Services;
+
+namespace FilomenoMauiMidterm.ViewModels
+{
+    public partial class SearchViewModel:ObservableObject
+    {
+
+        UserService _userService;
+        [ObservableProperty]
+        ObservableCollection<User> _users;
+        [ObservableProperty]
+        bool _isBusy;
+        [ObservableProperty]
+        bool _isNotBusy;
+        [ObservableProperty]
+        ObservableCollection<User> _searchedUsers;
+        NavigationId _navigationId;
+        CancellationToken _cancellationToken;
+        [ObservableProperty]
+        string _searchQuery;
+
+        public SearchViewModel(UserService userService, NavigationId navigationId)
+        {
+            _userService = userService;
+
+            _navigationId = navigationId;
+            SearchQuery = string.Empty;
+        }
+        public void FilterUsers(string searchText)
+        {
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                SearchedUsers = new ObservableCollection<User>();
+            }
+            else
+            {
+                var filteredUsers = Users
+                    .Where(user =>
+                        user.FirstName.Contains(searchText, StringComparison.OrdinalIgnoreCase) ||
+                        user.LastName.Contains(searchText, StringComparison.OrdinalIgnoreCase) ||
+                        user.Username.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+
+                SearchedUsers = new ObservableCollection<User>(filteredUsers);
+            }
+        }
+
+        [RelayCommand]
+        private async Task GotoProfileView(string id)
+        {
+            _navigationId.Id = id;
+            await Shell.Current.GoToAsync("profile");
+        }
+        [RelayCommand]
+        private void ClearSearch()
+        {
+            SearchQuery = string.Empty;
+            //await Shell.Current.GoToAsync("//home");
+        }
+
+        public async Task LoadDataAsync(CancellationToken cancellationToken)
+        {
+            _cancellationToken = cancellationToken;
+            if (IsBusy) return;
+            IsBusy = true;
+            IsNotBusy = false;
+            try
+            {
+                _cancellationToken.ThrowIfCancellationRequested();
+            
+                var users = await _userService.GetUsers(_cancellationToken);
+
+                Users = new ObservableCollection<User>(users);
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.WriteLine($"Canclled task");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+                IsNotBusy = true;
+            }
+        }
+
+
+    }
+}
--- FilomenoMauiMidterm/Views/Tabs/SearchView.xaml.cs
+++ FilomenoMauiMidterm/Views/Tabs/SearchView.xaml.cs
@@ -1,9 +1,49 @@
+using System.Diagnostics;
+using FilomenoMauiMidterm.ViewModels;
+
 namespace FilomenoMauiMidterm.Views.Tabs;
 
 public partial class SearchView : ContentPage
 {
-	public SearchView()
+	SearchViewModel _searchViewModel;
+    CancellationTokenSource _cancellationTokenSource;
+    CancellationTokenSource _debounceTokenSource;
+    public SearchView(SearchViewModel searchViewModel)
 	{
 		InitializeComponent();
+		_searchViewModel = searchViewModel;
+		BindingContext = _searchViewModel;
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        _cancellationTokenSource = new CancellationTokenSource();
+        try
+        {
+            await _searchViewModel.LoadDataAsync(_cancellationTokenSource.Token);
+
+        }
+        catch (OperationCanceledException)
+        {
+
+            Debug.Write("Operation Cancelled");
+        }
+
+    }
+
+    private async void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        _debounceTokenSource?.Cancel();
+        _debounceTokenSource = new CancellationTokenSource();
+        try
+        {
+            await Task.Delay(300, _debounceTokenSource.Token);
+            _searchViewModel.FilterUsers(e.NewTextValue);
+        }
+        catch (TaskCanceledException)
+        {
+            // Ignore cancellation
+        }
+    }
 }
